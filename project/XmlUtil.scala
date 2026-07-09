9a91405dc352189686910eb20266ea076d9b6025
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-09 21:12:52 +0200

    sbt 2.0.1 (was 1.12.13) (#733)
    Co-authored-by: Cédric Chantepie <cchantep@users.noreply.github.com>

--- project/XmlUtil.scala
+++ project/XmlUtil.scala
@@ -6,7 +6,7 @@ import scala.xml.{ Elem => XmlElem, Node => XmlNode, NodeSeq, XML }
 import scala.xml.transform.{ RewriteRule, RuleTransformer }
 
 object XmlUtil {
-  def transformPomDependencies(tx: XmlElem => Option[XmlNode]): XmlNode => XmlNode = { node: XmlNode =>
+  def transformPomDependencies(tx: XmlElem => Option[XmlNode]): XmlNode => XmlNode = { (node: XmlNode) =>
     val tr = new RuleTransformer(new RewriteRule {
       override def transform(node: XmlNode): NodeSeq = node match {
         case e: XmlElem if e.label == "dependency" =>
